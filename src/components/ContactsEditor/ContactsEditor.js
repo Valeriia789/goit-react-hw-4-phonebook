e1e7e974b487a2d9dfc575ba2388b256b87d2dce
Valeriia <valeriia.air@gmail.com>
2022-08-30 01:28:11 +0300

    робота з хуками

--- src/components/ContactsEditor/ContactsEditor.js
+++ src/components/ContactsEditor/ContactsEditor.js
@@ -1,43 +1,40 @@
-import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+
+import useLocalStorage from '../../hooks/useLocalStorage';
 
 import { Form, Label, SubmitButton } from './ContactsEditor.styled'
 import { Input } from '../commonStyles/Input.styled'
 
 const ContactsEditor = () => {
-  // ?? - заменяет || при присвоении значения,
-  //  правую часть вернет только в том случае, если слева стоит undefined или null
-  const [contactName, setContactName] = useState(
-    JSON.parse(window.localStorage.getItem('contactName')) ?? ''
-  )
-  const [contactNumber, setContactNumber] = useState(
-    JSON.parse(window.localStorage.getItem('contactNumber')) ?? ''
-  )
+  const [name, setName] = useLocalStorage('name', '')
+  const [number, setNumber] = useLocalStorage('number', '')
 
   const handleChange = event => {
     const { name, value } = event.target
+    console.log(name);
+    console.log(value);
 
     switch (name) {
       case 'contactName':
-        setContactName(value)
+        setName(value)
         break
       case 'contactNumber':
-        setContactNumber(value)
+        setNumber(value)
         break
       default:
         return
     }
   }
 
-  useEffect(() => {
-    console.log('contactName useEffect')
-    window.localStorage.setItem('contactName', JSON.stringify[contactName])
-  }, [contactName])
+  // useEffect(() => {
+  //   console.log('contactName useEffect')
+  //   window.localStorage.setItem('contactName', JSON.stringify[contactName])
+  // }, [contactName])
 
-  useEffect(() => {
-    console.log('contactNumber useEffect')
-    window.localStorage.setItem('contactNumber', JSON.stringify[contactNumber])
-  }, [contactNumber])
+  // useEffect(() => {
+  //   console.log('contactNumber useEffect')
+  //   window.localStorage.setItem('contactNumber', JSON.stringify[contactNumber])
+  // }, [contactNumber])
 
   const handleSubmit = event => {
     event.preventDefault()
@@ -52,7 +49,7 @@ const ContactsEditor = () => {
         <Input
           type='text'
           name='contactName'
-          value={contactName}
+          value={name}
           onChange={handleChange}
           placeholder='Name Surname'
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
@@ -66,7 +63,7 @@ const ContactsEditor = () => {
         <Input
           type='tel'
           name='contactNumber'
-          value={contactNumber}
+          value={number}
           onChange={handleChange}
           placeholder='+38 000 00 00'
           pattern='\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
@@ -85,3 +82,17 @@ ContactsEditor.propTypes = {
 }
 
 export default ContactsEditor
+
+  // ?? - заменяет || при присвоении значения,
+  //  правую часть вернет только в том случае, если слева стоит undefined или null
+  
+// так погано:
+// const [contactNumber, setContactNumber] = useState(
+//   JSON.parse(window.localStorage.getItem('contactNumber')) ?? ''
+// )
+
+// так добре (лінива ініціалізація початкового значення,
+//    функція буде викликана лише один раз, під час першого рендеру) :
+//    const [contactName, setContactName] = useState(() => {
+//     return JSON.parse(window.localStorage.getItem('contactName')) ?? ''
+//   })
