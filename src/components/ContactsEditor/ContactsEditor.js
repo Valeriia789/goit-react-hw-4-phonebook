38d54b1e3b5f9424a27e822792ec5cd2bf63d3e8
Valeriia <valeriia.air@gmail.com>
2022-08-30 01:28:05 +0300

    робота з хуками

--- src/components/ContactsEditor/ContactsEditor.js
+++ src/components/ContactsEditor/ContactsEditor.js
@@ -1,68 +1,87 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import { Form, Label, SubmitButton } from './ContactsEditor.styled'
 import { Input } from '../commonStyles/Input.styled'
 
-export default class ContactsEditor extends Component {
-  state = {
-    name: '',
-    number: ''
-  }
+const ContactsEditor = () => {
+  // ?? - заменяет || при присвоении значения,
+  //  правую часть вернет только в том случае, если слева стоит undefined или null
+  const [contactName, setContactName] = useState(
+    JSON.parse(window.localStorage.getItem('contactName')) ?? ''
+  )
+  const [contactNumber, setContactNumber] = useState(
+    JSON.parse(window.localStorage.getItem('contactNumber')) ?? ''
+  )
 
-  handleChange = event => {
+  const handleChange = event => {
     const { name, value } = event.target
 
-    this.setState({
-      [name]: value
-    })
+    switch (name) {
+      case 'contactName':
+        setContactName(value)
+        break
+      case 'contactNumber':
+        setContactNumber(value)
+        break
+      default:
+        return
+    }
   }
 
-  handleSubmit = event => {
+  useEffect(() => {
+    console.log('contactName useEffect')
+    window.localStorage.setItem('contactName', JSON.stringify[contactName])
+  }, [contactName])
+
+  useEffect(() => {
+    console.log('contactNumber useEffect')
+    window.localStorage.setItem('contactNumber', JSON.stringify[contactNumber])
+  }, [contactNumber])
+
+  const handleSubmit = event => {
     event.preventDefault()
-    this.props.onAddContact(this.state.name, this.state.number)
-    this.setState({ name: '', number: '' })
+    // this.props.onAddContact(this.state.name, this.state.number)
+    // this.setState({ name: '', number: '' })
   }
 
-  render () {
-    const { name, number } = this.state
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label>
+        Name
+        <Input
+          type='text'
+          name='contactName'
+          value={contactName}
+          onChange={handleChange}
+          placeholder='Name Surname'
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </Label>
 
-    return (
-      <Form onSubmit={this.handleSubmit}>
-        <Label>
-          Name
-          <Input
-            type='text'
-            name='name'
-            value={name}
-            onChange={this.handleChange}
-            placeholder='Name Surname'
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </Label>
+      <Label>
+        Phone number
+        <Input
+          type='tel'
+          name='contactNumber'
+          value={contactNumber}
+          onChange={handleChange}
+          placeholder='+38 000 00 00'
+          pattern='\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
+          title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+          required
+        />
+      </Label>
 
-        <Label>
-          Phone number
-          <Input
-            type='tel'
-            name='number'
-            value={number}
-            onChange={this.handleChange}
-            placeholder='+38 000 00 00'
-            pattern='\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}'
-            title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-            required
-          />
-        </Label>
-
-        <SubmitButton type='submit'>Add contact</SubmitButton>
-      </Form>
-    )
-  }
+      <SubmitButton type='submit'>Add contact</SubmitButton>
+    </Form>
+  )
 }
 
 ContactsEditor.propTypes = {
-  onAddContact: PropTypes.func.isRequired
+  // onAddContact: PropTypes.func.isRequired
 }
+
+export default ContactsEditor
